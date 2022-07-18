2f205100d07392e2148ba356e1ee2b3e7b4c2684
debola_is <adebolasanni079@gmail.com>
2022-07-18 12:05:58 +0100

    Worked on the dynamoDB paginaton exercise

--- src/components/dynamodb-pagination.js
+++ src/components/dynamodb-pagination.js
@@ -1,18 +1,109 @@
 const AWS = require ("aws-sdk")
 const docClient = new AWS.DynamoDBDocumentClient()
+const groupsTable = process.env.GROUP_TABLE
 
-const result = await docClient.scan({
-    TableName: process.env.GROUP_TABLE,
-    Limit: 2
-}).promise()
 
-const items = result.Items
-const nextKey = result.LastEvakluatedKey
 
-if(nextKey != null) {
-    const moreData = await docClient.scan({
-        TableName: process.env.GROUP_TABLE,
-        Limit: 3,
-        ExlclusiveStartKey: nextKey
-    }).promise()
+exports.handler = async (event) => {
+    console.log('Processing event: ', event)
+    let limit
+    let nextKey
+    
+    try {
+        limit = parseLimitParameter(event) || 5
+        nextKey = parseNextKeyParameter(event)
+    }
+    catch(e) {
+        console.log("Failed to parse query parameters")
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+                error: "Invalid query parameters"
+            })
+        }
+    }
+    
+    // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
+    // let nextKey // Next key to continue scan operation if necessary
+    // let limit // Maximum number of elements to return
+  
+    // HINT: You might find the following method useful to get an incoming parameter value
+    // getQueryParameter(event, 'param')
+  
+    // TODO: Return 400 error if parameters are invalid
+  
+    // Scan operation parameters
+    const scanParams = {
+      TableName: groupsTable,
+      Limit: 3,
+      ExlclusiveStartKey: nextKey,
+    }
+    console.log('Scan params: ', scanParams)
+  
+    const result = await docClient.scan(scanParams).promise()
+  
+    const items = result.Items
+  
+    console.log('Result: ', result)
+  
+    // Return result
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        items,
+        // Encode the JSON object so a client can return it in a URL as is
+        nextKey: encodeNextKey(result.LastEvaluatedKey)
+      })
+    }
+  }
+
+
+
+//Encodes value of NextKey as this value has to be encoded in our response body
+  function encodeNextKey(lastEvaluatedKey) {
+    if (!lastEvaluatedKey) {
+      return null
+    }
+  
+    return encodeURIComponent(JSON.stringify(lastEvaluatedKey))
+  }
+
+// This function obtains the value of the nextKey from the query parameters,
+//decodes it and returns the value as a JSON object.
+
+function parseNextKeyParameter(event) {
+    const nextKeyStr = getQueryParameters(event, 'nextKey')
+    if (!nextKeyStr){
+        return undefined
+    }
+
+    const decodedValue = decodeURIComponent(nextKeyStr)
+    return JSON.parse(decodedValue)
 }
+
+//This funtion obtains the value of limit from the query parameters,
+//and returns it as a more useful integer
+
+function parseLimitParameter(event) {
+    const limitStr = getQueryParameters(event, 'limit')
+    if (!limitStr){
+        return undefined
+    }
+
+    //convert the limit gotten from the query parameters to type int
+    const limit = parseInt(limitStr, 10)
+    
+    if(limit <=0 || limit != Math.floor(limit)){
+        throw new Error ("Limit should be a positive integer!")
+    }
+
+    return limit
+    
+}
+  
